3a8149bc9e99d74acae51529ed6ea956d0eb6aa8
Kunal Arora <90236283+kunalarora0930@users.noreply.github.com>
2023-07-04 23:23:20 +0530

    Initial commit | created routes

---
+++ index.js
@@ -0,0 +1,65 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+//import api routes-----------------------------------------------------------------------
+import userRoutes from './routes/userRoutes.js';
+import productRoutes from './routes/productRoutes.js';
+import cartRoutes from './routes/cartRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
+import reviewRoutes from './routes/reviewRoutes.js';
+import categoryRoutes from './routes/categoryRoutes.js';
+import wishlistRoutes from './routes/wishlistRoutes.js';
+import paymentRoutes from './routes/paymentRoutes.js';
+import shippingRoutes from './routes/shippingRoutes.js';
+import searchRoutes from './routes/searchRoutes.js';
+import analyticsRoutes from './routes/analyticsRoutes.js';
+//--------------api routes^-------------------------------------------------------------------
+
+
+dotenv.config();
+
+// Create an instance of Express
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+//api routes
+app.use('/api/users', userRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/cart', cartRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/reviews', reviewRoutes);
+app.use('/api/categories', categoryRoutes);
+app.use('/api/wishlist', wishlistRoutes);
+app.use('/api/payment', paymentRoutes);
+app.use('/api/shipping', shippingRoutes);
+app.use('/api/search', searchRoutes);
+app.use('/api/analytics', analyticsRoutes);
+
+
+
+// MongoDB connection
+const dbURL = process.env.MONGODB_URL;
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('MongoDB connection error:', error);
+  });
+
+// Define your routes
+app.get('/', (req, res) => {
+  res.send('Welcome to your eCommerce store backend!');
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ models/Product.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  // Additional fields...
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+export default Product;
